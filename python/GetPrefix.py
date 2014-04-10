51f6c3b26bd2b58eabd63f010694b31e68772b39
Gryte Satas <gsatas@cs.brown.edu>
2014-04-10 14:26:34 -0400

    Added GetPrefix.py

---
+++ python/GetPrefix.py
@@ -0,0 +1,9 @@
+from FileIO import *
+
+filename,results,n,k,tau,directory,prefix,max_normal,bound_heuristic,normal_bound_heuristic, heuristic_lb, heuristic_ub, num_processes, bounds_only, multi_event, force, get_values, interval_selection, num_intervals = parse_arguments(silent = True)
+
+#runN3 = os.path.join(directory, prefix+".RunN3.bash")
+prefix = os.path.join(directory,prefix)
+
+print prefix
+
