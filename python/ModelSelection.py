28b43fdd3d14fbb60cee0c8c469b43be261b2dcc
Gryte Satas <gsatas7@gmail.com>
2015-08-20 18:20:27 -0400

    Moved model selection code into a method within the file

--- python/ModelSelection.py
+++ python/ModelSelection.py
@@ -24,81 +24,90 @@
 # @author Layla Oesper, Ahmad Mahmoody, Benjamin J. Raphael and Gryte Satas
 ###
 
+
 import sys
-import math 
 import os.path
-
-inputFile = sys.argv[1]
-n2Result = sys.argv[2]
-n3Result = sys.argv[3]
-
-###
-#	Read Result File to Obtain #Tumor Reads, #Normal Reads, #Intervals
-###
-numTumor = 0
-numNormal = 0 
-numIntervals = 0
-with open(inputFile) as f:
-	for line in f:
-		if line.startswith("#"): continue
-		tumor, normal = line.strip().split("\t")[4:6]
-
-		if int(normal) > 0:
-			numTumor += int(tumor)
-			numNormal += int(normal)
-			numIntervals += 1
-
-###
-#	Obtain NLL from n = 2
-###
-n2NLL= float("inf")
-with open(n2Result) as f:
-	for line in f:
-		if line.startswith("#"): continue
-		likelihood = float(line.strip().split("\t")[0])
-		if likelihood < n2NLL: n2NLL = likelihood
-
-###
-#	Obtain NLL from n = 3
-###
-n3NLL= float("inf")
-with open(n3Result) as f:
-	for line in f:
-		if line.startswith("#"): continue
-		likelihood = float(line.strip().split("\t")[0])
-		if likelihood < n3NLL: n3NLL = likelihood
+import math 
 
 
-###
-#	Penalized NLLs
-#	P-NLL = 2*L + (m+1)(n-1)log(T+N) 
-###
-P_NLL_N2 = 2*n2NLL + (numIntervals+1) * math.log(numTumor + numNormal)
-P_NLL_N3 = 2*n3NLL + (numIntervals+1) * 2 * math.log(numTumor + numNormal)
+def ModelSelection(inputFile, n2Result, n3Result):
+	
+	###
+	#	Read Result File to Obtain #Tumor Reads, #Normal Reads, #Intervals
+	###
+	numTumor = 0
+	numNormal = 0 
+	numIntervals = 0
+	with open(inputFile) as f:
+		for line in f:
+			if line.startswith("#"): continue
+			tumor, normal = line.strip().split("\t")[4:6]
+	
+			if int(normal) > 0:
+				numTumor += int(tumor)
+				numNormal += int(normal)
+				numIntervals += 1
+	
+	###
+	#	Obtain NLL from n = 2
+	###
+	n2NLL= float("inf")
+	with open(n2Result) as f:
+		for line in f:
+			if line.startswith("#"): continue
+			likelihood = float(line.strip().split("\t")[0])
+			if likelihood < n2NLL: n2NLL = likelihood
+	
+	###
+	#	Obtain NLL from n = 3
+	###
+	n3NLL= float("inf")
+	with open(n3Result) as f:
+		for line in f:
+			if line.startswith("#"): continue
+			likelihood = float(line.strip().split("\t")[0])
+			if likelihood < n3NLL: n3NLL = likelihood
+	
+	
+	###
+	#	Penalized NLLs
+	#	P-NLL = 2*L + (m+1)(n-1)log(T+N) 
+	###
+	P_NLL_N2 = 2*n2NLL + (numIntervals+1) * math.log(numTumor + numNormal)
+	P_NLL_N3 = 2*n3NLL + (numIntervals+1) * 2 * math.log(numTumor + numNormal)
+	
+	from subprocess import call
+	
+	if P_NLL_N2 < P_NLL_N3:
+		filename = n2Result.replace(".n2.results", ".BEST.results")
+		print "Selected n=2 solution. Writing result to", filename,
+		with open(filename, 'w') as out, open(n2Result) as n2In:
+			for line in n2In:
+				out.write(line)
+		pdfFileN2 = n2Result + ".pdf"
+		pdfFileBest = filename + ".pdf"
+		if os.path.isfile(pdfFileN2):
+			call(["cp", pdfFileN2, pdfFileBest])
+			print ",", pdfFileBest
+		else: print ""
+	else:
+		filename = n3Result.replace(".n3.results", ".BEST.results")
+		print "Selected n=3 solution. Writing result to", filename,
+		with open(filename, 'w') as out, open(n3Result) as n3In:
+			for line in n3In:
+				out.write(line)
+		pdfFileN3 = n3Result + ".pdf"
+		pdfFileBest = filename + ".pdf"
+		if os.path.isfile(pdfFileN3):
+			call(["cp", pdfFileN3, pdfFileBest])
+			print ",", pdfFileBest
+		else: print ""
 
-from subprocess import call
 
-if P_NLL_N2 < P_NLL_N3:
-	filename = n2Result.replace(".n2.results", ".BEST.results")
-	print "Selected n=2 solution. Writing result to", filename,
-	with open(filename, 'w') as out, open(n2Result) as n2In:
-		for line in n2In:
-			out.write(line)
-	pdfFileN2 = n2Result + ".pdf"
-	pdfFileBest = filename + ".pdf"
-	if os.path.isfile(pdfFileN2):
-		call(["cp", pdfFileN2, pdfFileBest])
-		print ",", pdfFileBest
-	else: print ""
-else:
-	filename = n3Result.replace(".n3.results", ".BEST.results")
-	print "Selected n=3 solution. Writing result to", filename,
-	with open(filename, 'w') as out, open(n3Result) as n3In:
-		for line in n3In:
-			out.write(line)
-	pdfFileN3 = n3Result + ".pdf"
-	pdfFileBest = filename + ".pdf"
-	if os.path.isfile(pdfFileN3):
-		call(["cp", pdfFileN3, pdfFileBest])
-		print ",", pdfFileBest
-	else: print ""
+if __name__ == '__main__':
+	inputFile = sys.argv[1]
+	n2Result = sys.argv[2]
+	n3Result = sys.argv[3]
+
+	ModelSelection(inputFile, n2Result, n3Result)
+
